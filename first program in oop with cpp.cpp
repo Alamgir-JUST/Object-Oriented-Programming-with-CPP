20aa04757ea816b78a991624d0e5b16c79a87b68
ALamgir Hossain <alamgir.cse14.just@gmail.com>
2022-10-11 22:56:32 +0600

    Add files via upload

---
+++ first program in oop with cpp.cpp
@@ -0,0 +1,16 @@
+//First C++ program using the concept of OOP
+#include<iostream>
+using namespace std;
+class first{
+public:
+    void check(){
+        cout<<"Hey! I am Checking Function"<<endl;
+    }
+};
+int main()
+{
+    first obj;
+    obj.check();
+    return 0;
+}
+
