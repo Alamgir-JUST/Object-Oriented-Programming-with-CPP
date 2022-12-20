1223e5420e6b133dab2211b8529afb86f6e8b9c9
Alamgir Hossain <alamgir.cse14.just@gmail.com>
2022-12-20 23:47:32 +0600

    Add files via upload

---
+++ special case.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class parent{
+public:
+    parent(int number){
+        cout<<"Value of the number from parent class is: "<<number<<endl;
+    }
+};
+class child: public parent{
+public:
+    using parent::parent;//Adding the parent constructor to this scope
+    child(): parent(10){
+    }
+};
+class child2: public child{
+public:
+    using child::child;
+    child2(): child(10){
+    }
+};
+int main()
+{
+    child2 ob(100);
+    return 0;
+}
+//Output: Value of the number from parent class is: 100
+
+
